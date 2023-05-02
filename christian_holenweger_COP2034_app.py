f6d5d99ea56506cff80bd497012c8aee721d5779
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 04:14:41 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -5,13 +5,13 @@ import io
 import streamlit as st
 
 # Set up the app title and description
-st.title("My Streamlit App")
-st.sidebar.write("Welcome to my app! This is a demo of how to create a simple layout with Streamlit.")
+st.title("Filters on filters")
+st.sidebar.write("Web App created using Python Streamlit library. This app supports ('jpg','png',jpeg') files!.")
 
 # Set up the center column with Name, Project, and Due Date
-st.header("Name: John Smith")
-st.header("Project: Image Classifier")
-st.header("Due Date: May 31, 2023")
+st.header("Name: Christian Holenweger")
+st.header("Project: Filter Changer")
+st.header("Due Date: May 3, 2023")
 
 # Set up the right column with the FAU owl logo
 image = "https://www.fau.edu/president/assets/images/owl.svg"
