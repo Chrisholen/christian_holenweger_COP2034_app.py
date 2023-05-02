37c0fe317d23dbb5cdf4b6b4b8b4ee0895a4bcaf
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 04:34:04 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -1,26 +1,36 @@
 import streamlit as st
-from PIL import Image, ImageFilter
-import io
-from PIL import Image
+import pandas as pd
+import numpy as np
 
-# App Name and Description
-st.sidebar.title("Filters on filters")
-st.sidebar.write("This is a app that lets the user change how an uploaded image looks by using filters to change the apperence.")
-st.sidebar.write("Web App created using Python Streamlit library. This app supports ('jpg','png',jpeg') files!")
+st.title('Uber pickups in NYC')
 
-# Full Name, Project, and Due Date
-st.title("Filters on filters")
-st.write("Created by: Chrisitan Holenweger")
-st.write("Project: Filters on Filters")
-st.write("Due Date: 05/03/2023")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-# FAU owl logo
-image = Image.open('fau_owl.png')
-st.sidebar.image(image, caption='', use_column_width=True)
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-# File uploader to upload an image
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
