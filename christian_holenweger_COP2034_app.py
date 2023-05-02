75321067ba98c8acf84065caa00ba2659ccede65
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 09:31:32 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -3,11 +3,10 @@ from PIL import Image
 import numpy as np
 
 # App Name and Description
-st.leftsidebar.title('Filters change pictures')
-st.leftsidebar.write('This is an app that lets the user change how an uploaded image looks by using filters to change the appearance.')
-st.leftsidebar.write('Web App created using Python Streamlit library. This app supports (.jpg, .png, .jpeg)')
+st.sidebar.title('Filters change pictures')
+st.sidebar.write('This is an app that lets the user change how an uploaded image looks by using filters to change the appearance.')
+st.sidebar.write('Web App created using Python Streamlit library. This app supports (.jpg, .png, .jpeg)')
 
-st.rightsidebar.write('project')
 
 # Full Name, Project, and Due Date
 st.title("Christian_Holenweger_COP2034_app.py!")
