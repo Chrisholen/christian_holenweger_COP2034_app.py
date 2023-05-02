5e3293962bdb46f2bbda804a92d6d0af9d793fa8
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 09:47:24 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -15,3 +15,12 @@ st.title("Due Date: 05/03/2023")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+# Set the file path to the FAU logo PNG file in your pictures directory
+file_path = "C:\Users\Christian Holenweger\pictures\directory\fau_logo.png"
+
+# Use PIL to open the image file
+image = Image.open(file_path)
+
+# Display the image in Streamlit
+st.image(image, caption="FAU Logo", use_column_width=True)
