c3f9ced58facd1cf14de0bab3adc645b7512512f
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 09:26:24 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -12,21 +12,6 @@ st.write("Created by: Your Name")
 st.write("Project: Your Project Name")
 st.write("Due Date: MM/DD/YYYY")
 
-# FAU owl logo
-image = Image.open('fau-owl.png')
-st.sidebar.image(image, caption='', use_column_width=True)
-
-# File uploader to upload an image
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-
-
-# Set page title and icon
-st.set_page_config(page_title="Image Filters", page_icon=":camera:")
-
 # Page title and description
 st.title('Filters on Filters')
 st.write('This is an app that lets the user change how an uploaded image looks by using filters to change the appearance.')
