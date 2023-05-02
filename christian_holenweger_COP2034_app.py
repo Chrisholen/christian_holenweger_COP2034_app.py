ea0ee2e2eebedc136d33f0f1b0a5cf55227c084a
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 04:38:33 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -2,7 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Christian Holenweger')
+st.title('Final Project COP2034')
+st.title('May 03, 2023')
+
+st.sidebar.title("Filters on Filters")
+st.sidebar.write("This is a app that lets the user change how an uploaded image looks by using filters to change the apperence.!")
+st.sidebar.write("Web App created using Python Streamlit library. this app supports ('jpg','png','jpeg')")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
