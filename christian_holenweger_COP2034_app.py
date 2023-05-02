695b7edcaeb4fd22b23ad1fabcf43b7fd0fe3050
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 05:00:53 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -11,3 +11,16 @@ st.sidebar.title("Filters on Filters")
 st.sidebar.write("This is a app that lets the user change how an uploaded image looks by using filters to change the apperence.!")
 st.sidebar.write("Web App created using Python Streamlit library. this app supports ('jpg','png','jpeg')")
 
+# Set page title
+st.set_page_config(page_title="Image Uploader")
+
+# Set page header
+st.title("Image Uploader")
+
+# Create an upload button
+uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
+
+# Display the uploaded image
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded image")
