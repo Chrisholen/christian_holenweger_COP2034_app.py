59573c7500602ffe9c1844c8ca1e908b11dca506
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 03:45:18 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -18,34 +18,3 @@ uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "p
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded image")
-
-# Function to apply grayscale filter
-def grayscale(img):
-    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Radio buttons to select filter
-filter_choice = st.radio("Select a filter", ("Grayscale",))
-
-# Apply selected filter
-if filter_choice == "Grayscale":
-    grayscale_image = grayscale(np.array(image))
-    st.image(grayscale_image, caption='Grayscale Image', use_column_width=True)
-# Function to apply brightness filter
-def brightness(img, brightness_level):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    lim = 255 - brightness_level
-    v[v > lim] = 255
-    v[v <= lim] += brightness_level
-    final_hsv = cv2.merge((h, s, v))
-    return cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-
-# Radio buttons to select filter
-filter_choice = st.radio("Select a filter", ("Grayscale", "Brightness"))
-
-# Apply selected filter
-if filter_choice == "Grayscale":
-    grayscale_image = grayscale(np.array(image))
-    st.image(grayscale_image, caption='Grayscale Image', use_column_width=True)
-elif filter_choice == "Brightness":
-    brightness_level = st.slider
