20dc5703fe7eff8f072debcdf44131dacd1b5d54
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 09:29:16 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -3,19 +3,14 @@ from PIL import Image
 import numpy as np
 
 # App Name and Description
-st.sidebar.title("My Streamlit App")
-st.sidebar.write("This is a simple app created using Streamlit!")
+st.sidebar.title('Filters on Filters')
+st.sidebar.write('This is an app that lets the user change how an uploaded image looks by using filters to change the appearance.')
+st.sidebar.write('Web App created using Python Streamlit library. This app supports (.jpg, .png, .jpeg)')
 
 # Full Name, Project, and Due Date
-st.title("Welcome to my Streamlit app!")
-st.write("Created by: Your Name")
-st.write("Project: Your Project Name")
-st.write("Due Date: MM/DD/YYYY")
-
-# Page title and description
-st.title('Filters on Filters')
-st.write('This is an app that lets the user change how an uploaded image looks by using filters to change the appearance.')
-st.write('Web App created using Python Streamlit library. This app supports (.jpg, .png, .jpeg)')
+st.title("Christian_Holenweger_COP2034_app.py!")
+st.write("Final Project COP2034")
+st.write("Due Date: 05/03/2023")
 
 # File uploader
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
