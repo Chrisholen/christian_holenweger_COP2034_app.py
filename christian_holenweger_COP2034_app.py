7e078e2cff71d7d637027a5c1454767023adaf75
Chrisholen <132319516+Chrisholen@users.noreply.github.com>
2023-05-02 09:25:42 -0400

    Update christian_holenweger_COP2034_app.py

--- christian_holenweger_COP2034_app.py
+++ christian_holenweger_COP2034_app.py
@@ -2,6 +2,28 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
+# App Name and Description
+st.sidebar.title("My Streamlit App")
+st.sidebar.write("This is a simple app created using Streamlit!")
+
+# Full Name, Project, and Due Date
+st.title("Welcome to my Streamlit app!")
+st.write("Created by: Your Name")
+st.write("Project: Your Project Name")
+st.write("Due Date: MM/DD/YYYY")
+
+# FAU owl logo
+image = Image.open('fau-owl.png')
+st.sidebar.image(image, caption='', use_column_width=True)
+
+# File uploader to upload an image
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+
 # Set page title and icon
 st.set_page_config(page_title="Image Filters", page_icon=":camera:")
 
